f03c3e689139cccf4b1387fbab3af555c89dcff6
Thomas Gehrke <tomgehrke@gmail.com>
2018-06-17 14:58:19 -0500

    Added setup project.

--- ActiveDirectoryUtilityLibrary/ActiveDirectoryUser.cs
+++ ActiveDirectoryUtilityLibrary/ActiveDirectoryUser.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ActiveDirectoryUtilityLibrary
 {
--- PET/MainForm.cs
+++ PET/MainForm.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using ActiveDirectoryUtilityLibrary;
+
+using System;
 using System.Drawing;
-using System.Linq;
 using System.Security.Principal;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using ActiveDirectoryUtilityLibrary;
 
 namespace Pet
 {
